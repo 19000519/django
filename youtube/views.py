11b0d3dbffab0943daa8f69a39865ce366dbea36
KOSWANE PHEMELO <KP19000519@BIUST.AC.BW>
2020-11-26 22:44:17 +0200

    update

--- youtube/views.py
+++ youtube/views.py
@@ -1,10 +1,10 @@
-
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
-from django.views.generic.base import HttpResponse, View 
+from django.views.generic.base import HttpResponse, View, HttpResponse
 from .forms import LoginForm, RegisterForm
 from youtube.models import Video
-
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 # Create your views here.
 class HomeView(View):
     template_name = 'Home.html'
@@ -17,11 +17,23 @@ class LoginView(View):
     template_name = 'login.html'
 
     def get(self, request):
+        if request.user.is_authenticated:
+           return HttpResponseRedirect('/')  
         form = LoginForm()
         return render(request, self.template_name, {'form': form})
 
     def post(self, request):
-        print('HELLO THIS POST')
+         #pass filled out HTML-form from view to LoginForm()
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username = username, password = password)
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect('/')  
+            else:
+                return HttpResponseRedirect('/login')
         return HttpResponse('this is login view. POST request')
 
 class RegisterView(View):
@@ -36,8 +48,14 @@ class RegisterView(View):
         form = RegisterForm(request.POST)
         if form.is_valid():
             #Create a user account
-             return HttpResponseRedirect('/thanks/')
-        return HttpResponse('this is register view. POST request')
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            email = form.cleaned_data['email']
+            new_user =User(username = username, password = password, email = email)
+            new_user.set_password(password)
+            new_user.save()
+            return HttpResponseRedirect('/login')
+        return HttpResponse()
 
 class NewVideo(View):
     video_class = Video
